25434cfb1c9658b9261d21ce532aeb2da002da7a
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2025-07-16 15:42:59 +0200

    [TASK] Fixed version constraints

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,19 +3,16 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Forum',
     'description' => 'Forum',
     'category' => 'plugin',
-    'author' => 'Stefan Froemken',
-    'author_email' => 'sfroemken@jweiland.net',
+    'author' => 'Stefan Froemken, Hoja Mustaffa Abdul Latheef',
+    'author_email' => 'projects@jweiland.net',
     'author_company' => 'jweiland.net',
     'state' => 'stable',
-    'clearCacheOnLoad' => 0,
-    'version' => '4.0.3',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.32-11.5.99',
+            'typo3' => '12.4.31-12.4.99',
         ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
 ];
